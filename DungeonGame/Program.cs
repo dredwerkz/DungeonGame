94fae98c97c70884e09f3b5a3020e77cc22b3621
dredwerkz <avoiding.the.mischief@gmail.com>
2024-09-18 23:39:16 +0100

    Hello World

---
+++ DungeonGame/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace DungeonGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello, World! Say something: ");
+            var readLine = Console.ReadLine();
+            Console.WriteLine($"You typed {readLine}");
+        }
+    }
+}
